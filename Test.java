56ad3503a91d418d8a6f4fc252a0e37de0d36b95
sandy <lsandeepr50@gmail.com>
2025-07-26 18:05:58 +0530

    commited all

--- Test.java
+++ Test.java
@@ -2,7 +2,12 @@ class Test {
     public static void main(String[] args) {
         System.out.println("hello-world");
     }
+<<<<<<< Updated upstream
     public void streak() {
         String streak = "";
+=======
+    public void show() {
+        int i = 20 
+>>>>>>> Stashed changes
     }
 }
